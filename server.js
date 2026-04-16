50d80dd30a662cfb7aaeb91da7c15a8d392f017d
tonkotsu514-cpu <tonkotsu514@gmail.com>
2026-04-17 08:04:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -135,6 +135,6 @@ setInterval(() => {
 
 
 const PORT = Number(process.env.PORT) || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log('Server listening on port ' + PORT);
 });
